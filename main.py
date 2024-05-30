937f66c4a4890148546edb30553a1bc5865b7c55
Paul Fannon <paul.fannon@wwt.com>
2024-05-30 14:42:49 -0500

    Serve jupyter notebook as html content

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from nbconvert import HTMLExporter
+from fastapi.responses import HTMLResponse
+import nbformat
 
 app = FastAPI()
 
+
+def convert_notebook_to_html(notebook_path):
+	with open(notebook_path) as f:
+		nb = nbformat.read(f, as_version=4)
+		html_exporter = HTMLExporter()
+		# html_exporter.template_file = 'basic'
+		(body, resources) = html_exporter.from_notebook_node(nb)
+		return body
+
 @app.get("/")
-def read_root():
-	return {"Hello": "World"}
+def serve_notebook():
+	html_content = convert_notebook_to_html('linear model using autograd.ipynb')
+	return HTMLResponse(content=html_content, status_code=200)
